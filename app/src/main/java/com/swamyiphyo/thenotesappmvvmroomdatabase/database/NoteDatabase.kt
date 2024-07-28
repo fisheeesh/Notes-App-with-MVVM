c45e7118125ca80d05d5abdb6138aac1413556ef
fisheeesh <swanphyo444@gmail.com>
2024-07-28 16:16:18 +0700

    room database added

--- app/src/main/java/com/swamyiphyo/thenotesappmvvmroomdatabase/database/NoteDatabase.kt
+++ app/src/main/java/com/swamyiphyo/thenotesappmvvmroomdatabase/database/NoteDatabase.kt
@@ -1,14 +1,35 @@
-package com.swamyiphyo.thenotesappmvvmroomdatabase.database
 
+package com.swamyiphyo.thenotesappmvvmroomdatabase. database
+
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
 import com.swamyiphyo.thenotesappmvvmroomdatabase.model.Note
 
 @Database(entities = [Note::class], version = 1)
-abstract class NoteDatabase {
+abstract class NoteDatabase : RoomDatabase(){
+    //which declare NoteDao interface instance
     abstract fun getNoteDao() : NoteDao
 
-
     companion object{
+        @Volatile
+        private var instance : NoteDatabase? = null
+        private val LOCK = Any()
 
+        operator fun invoke(context: Context) = instance ?:
+        synchronized(LOCK){
+            instance ?:
+            createDatabase(context).also {
+                instance = it
+            }
+        }
+
+        private fun createDatabase(context: Context) =
+            Room.databaseBuilder(
+                context.applicationContext,
+                NoteDatabase::class.java,
+                "note_db"
+            ).build()
     }
 }
