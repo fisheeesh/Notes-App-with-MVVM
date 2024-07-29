009098f3fec14832df796da33e2830354b4bc438
fisheeesh <swanphyo444@gmail.com>
2024-07-29 11:36:13 +0700

    Note View Model Factory ready

--- app/src/main/java/com/swamyiphyo/thenotesappmvvmroomdatabase/viewmodel/NoteViewModelFactory.kt
+++ app/src/main/java/com/swamyiphyo/thenotesappmvvmroomdatabase/viewmodel/NoteViewModelFactory.kt
@@ -1,4 +1,15 @@
 package com.swamyiphyo.thenotesappmvvmroomdatabase.viewmodel
 
-class NoteViewModelFactory {
+import android.app.Application
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.swamyiphyo.thenotesappmvvmroomdatabase.repository.NoteRepository
+
+@Suppress("UNCHECKED_CAST")
+class NoteViewModelFactory(val app : Application, private val noteRepo : NoteRepository) : ViewModelProvider.Factory{
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return NoteViewModel(app, noteRepo) as T
+
+    }
 }
